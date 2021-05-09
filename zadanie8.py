fddd8d90eebcd855788e5e182c8688bcb424a949
TPawelek <TPawelek@users.noreply.github.com>
2021-05-09 11:54:44 +0000

    Zadanie 8

---
+++ zadanie8.py
@@ -0,0 +1,11 @@
+imiona = ['Tomek','Ewa','Michal', 'Monika', 'Konrad']
+
+print(imiona[0])
+print(imiona[1])
+print(imiona[2])
+
+for i in imiona:
+    print(i)
+
+for j in range(0,3):
+    print("idx: "+ str(j) +" : " + imiona[j])
